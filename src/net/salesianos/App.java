7e4a23f93f0bd3c8a0f12f51f74f48437ecdf4da
enriquedlc <enrique.delaconcepcion@driverevel.com>
2026-03-31 18:44:02 +0200

    personaje viajero en App

--- src/net/salesianos/App.java
+++ src/net/salesianos/App.java
@@ -1,6 +1,22 @@
 package net.salesianos;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        // a. Crear personaje viajero
+        Personaje viajero = new Personaje("Eldrin", 3);
+
+        // b. Entrenar 4 niveles
+        viajero.entrenar(4);
+
+        // c. Gastar 30 de energia
+        viajero.gastarEnergia(30);
+
+        // d. Mostrar informacion del personaje
+        viajero.mostrarInfo();
+
+        // e. Mostrar total de personajes
+        Personaje.mostrarTotalPersonajes();
+
     }
 }
